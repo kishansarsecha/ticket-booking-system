209cb375d3577a51a8d6fa4a693c4ba04bb4f82c
kishansarsecha <kishans@thoughtworks.com>
2021-08-10 11:08:07 +0530

    Step 1

--- app/app.go
+++ app/app.go
@@ -2,15 +2,25 @@ package app
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/sujithps/ticket-booking-system/app/handler"
 	"net/http"
 )
 
 func StartServer() {
-	if err := http.ListenAndServe(":8080", pingHandler()); err != nil && err != http.ErrServerClosed {
-		fmt.Errorf("something went wrong %s", err)
+
+	server := gin.Default()
+
+	server.GET("/book", func(context *gin.Context) {
+			context.String(http.StatusCreated, handler.BookTicket())
+	})
+
+	if err := server.Run(":8080"); err != nil {
+		_ = fmt.Errorf("something went wrong %s", err)
 		fmt.Println("Server Not Started")
 		return
 	}
+
 	fmt.Println("Server Started")
 }
 
---
+++ app/handler/book-ticket-handler.go
@@ -0,0 +1,5 @@
+package handler
+
+func BookTicket() string {
+	return "Ticket created"
+}
